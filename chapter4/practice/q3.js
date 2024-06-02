175e78d86d46dcfd00ea7cd06d2533607e111830
red0074 <pdinesh122003@gmail.com>
2024-06-02 12:38:55 +0530

    practice questions on array methods by manipulating array

---
+++ chapter4/practice/q3.js
@@ -0,0 +1,8 @@
+let companies = ["bloomrang", "microsft", "uber", "google", "ibm", "netflix"];
+console.log(companies);
+console.log(companies.shift());
+console.log(companies);
+console.log(companies.splice(1, 1, "ola"));
+console.log(companies);
+console.log(companies.push("amazon"));
+console.log(companies);
