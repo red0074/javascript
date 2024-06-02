800f3849f44e6e1328911fac411cf1f057126256
red0074 <pdinesh122003@gmail.com>
2024-06-02 12:27:34 +0530

    looping ,indexing,creation,methods in arrays of javascript

--- chapter4/script.js
+++ chapter4/script.js
@@ -22,3 +22,41 @@ for (let el of heros) {
 for (let hero of heros) {
   console.log(hero.toUpperCase());
 }
+
+//array methods
+let veggies = ["potato", "tomato", "brinjal", "carrot"];
+console.log(veggies);
+//->push()
+veggies.push("beetroot");
+console.log(veggies);
+//->pop()
+console.log(veggies.pop());
+console.log(veggies);
+//->toString()
+console.log(veggies.toString());
+//->concat():
+let marvel = ["ironman", "spiderman", "thor"];
+let dc = ["superman", "flash", "batman"];
+let cheros = marvel.concat(dc);
+console.log(cheros);
+//->unshift
+console.log(marvel);
+marvel.unshift("antman");
+console.log(marvel);
+//->shift
+console.log(marvel.shift());
+console.log(marvel);
+//->slice
+console.log(cheros);
+console.log(cheros.slice(1));
+console.log(cheros.slice(3, 6));
+//->splice\
+console.log(cheros);
+console.log(cheros.splice(2, 2, "haweye", "falcon")); //returns at that indeices
+console.log(cheros); //return with new array
+console.log(cheros.splice(2, 0, "thor", "superman")); //just to add at index
+console.log(cheros);
+console.log(cheros.splice(3, 1)); //just to delete at that index and returns
+console.log(cheros);
+console.log(cheros.splice(3, 1, "captian marvel")); //just to replace at index
+console.log(cheros);
